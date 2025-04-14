a47a5315d6594c8e757c40eef1de85e91e30dc19
Sailesh R <sailesh.r@mathco.com>
2025-04-14 14:00:27 +0530

    added a new line and deleted the sample.txt file

--- calculator.py
+++ calculator.py
@@ -1,6 +1,7 @@
 class clc:
     print("Welcome to the calculator")
     def add(a,b):
+        print("this is a new line added as an update addition 1.1")
         return a+b
     def subtract(a,b):
         return a-b
